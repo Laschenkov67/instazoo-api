da7ee14e17aa13deef0ad93311cc7442d1d631ee
Artem Laschenkov <laschenkov59@yandex.ru>
2023-01-04 14:50:04 +0300

    FEAT | INSTAZOO-API-9 | User модель

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,28 @@
 package entity;
 
+import entity.enums.ERole;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+    private Long id;
+    private String name;
+    private String username;
+    private String lastname;
+    private String email;
+    private String bio;
+    private String password;
+
+    private Set<ERole> role = new HashSet<>();
+    private List<Post> post = new ArrayList<>();
+    private LocalDateTime createDate;
+
+    /** Отслеживаем когда создан пользователь **/
+    protected void onCreateUser() {
+        this.createDate = LocalDateTime.now();
+    }
 }
