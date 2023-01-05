75ff98994184ec9ad0f35912ca4461bafb750ae0
Artem Laschenkov <laschenkov59@yandex.ru>
2023-01-05 22:10:08 +0300

    FEAT | INSTAZOO-API-13 | User аннотации

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,7 +1,9 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import entity.enums.ERole;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,17 +11,34 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> post = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
     /** Отслеживаем когда создан пользователь **/
