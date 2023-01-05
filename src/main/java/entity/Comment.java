41c105978948e9b02dc5b1f613d32a67fee0583a
Artem Laschenkov <laschenkov59@yandex.ru>
2023-01-05 22:31:49 +0300

    FEAT | INSTAZOO-API-15 | Comment аннотации

--- src/main/java/entity/Comment.java
+++ src/main/java/entity/Comment.java
@@ -1,15 +1,25 @@
 package entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createDate;
 
     @PrePersist
