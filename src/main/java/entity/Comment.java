e0e83b6393d7dfffd05e55de4cc46a994bb37dd8
Artem Laschenkov <laschenkov59@yandex.ru>
2023-01-05 21:45:25 +0300

    FEAT | INSTAZOO-API-12 | Comment модель

--- src/main/java/entity/Comment.java
+++ src/main/java/entity/Comment.java
@@ -1,4 +1,19 @@
 package entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String message;
+    private LocalDateTime createDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now();
+    }
 }
